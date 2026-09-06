01dbcf5ac3d35061f2a9301fa851d60dff54a52c
Workwithaditya01 <workswithaditya01@gmail.com>
2026-09-06 11:45:57 +0530

    Added variables.tf

---
+++ Day63/variables.tf
@@ -0,0 +1,46 @@
+variable "region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "allowed_ports" {
+  description = "Ports allowed through the security group"
+  type        = list(number)
+  default     = [22, 80, 443]
+}
+
+variable "extra_tags" {
+  description = "Additional tags for resources"
+  type        = map(string)
+  default     = {}
+}
