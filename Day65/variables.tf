cbb8624bd147d1caa0b620d51237ff2592989131
Workwithaditya01 <workswithaditya01@gmail.com>
2026-09-08 23:25:50 +0530

    added variables.tf

---
+++ Day65/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
