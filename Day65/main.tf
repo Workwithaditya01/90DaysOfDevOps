779116d286685c235ae8c6a7f1aac3932da8adf8
Workwithaditya01 <workswithaditya01@gmail.com>
2026-09-12 10:38:20 +0530

    Added root files

--- Day65/main.tf
+++ Day65/main.tf
@@ -1,39 +1,11 @@
 locals {
   common_tags = {
     Project     = "TerraWeek"
-    Day         = "Day-65"
+    Day         = "Day65"
     Environment = var.environment
     ManagedBy   = "Terraform"
   }
 }
-
-# ---------------------------------------------------------
-# Amazon Linux 2023 AMI
-# ---------------------------------------------------------
-
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["al2023-ami-*-x86_64"]
-  }
-
-  filter {
-    name   = "state"
-    values = ["available"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-}
-
-# ---------------------------------------------------------
-# VPC - Terraform Registry Module
-# ---------------------------------------------------------
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
@@ -57,54 +29,54 @@ module "vpc" {
     "10.0.4.0/24"
   ]
 
+  map_public_ip_on_launch = true
+
   enable_nat_gateway   = false
   enable_dns_hostnames = true
 
   tags = local.common_tags
 }
 
-# ---------------------------------------------------------
-# Custom Security Group Module
-# ---------------------------------------------------------
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
 module "web_sg" {
-  source = "./modules/security-group"
-
+  source        = "./modules/security-group"
   vpc_id        = module.vpc.vpc_id
   sg_name       = "terraweek-web-sg"
   ingress_ports = [22, 80, 443]
-
-  tags = local.common_tags
+  tags          = local.common_tags
 }
 
-# ---------------------------------------------------------
-# Custom EC2 Module - Web Server
-# ---------------------------------------------------------
-
 module "web_server" {
-  source = "./modules/ec2-instance"
-
+  source             = "./modules/ec2-instance"
   ami_id             = data.aws_ami.amazon_linux.id
-  instance_type      = "t2.micro"
+  instance_type      = "t3.micro"
   subnet_id          = module.vpc.public_subnets[0]
   security_group_ids = [module.web_sg.sg_id]
   instance_name      = "terraweek-web"
-
-  tags = local.common_tags
+  tags               = local.common_tags
 }
 
-# ---------------------------------------------------------
-# Custom EC2 Module - API Server
-# ---------------------------------------------------------
-
 module "api_server" {
-  source = "./modules/ec2-instance"
-
+  source             = "./modules/ec2-instance"
   ami_id             = data.aws_ami.amazon_linux.id
-  instance_type      = "t2.micro"
+  instance_type      = "t3.micro"
   subnet_id          = module.vpc.public_subnets[0]
   security_group_ids = [module.web_sg.sg_id]
   instance_name      = "terraweek-api"
-
-  tags = local.common_tags
+  tags               = local.common_tags
 }
--- Day65/outputs.tf
+++ Day65/outputs.tf
@@ -1,44 +1,19 @@
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = module.vpc.vpc_id
-}
-
-output "public_subnet_id" {
-  description = "ID of the first public subnet"
-  value       = module.vpc.public_subnets[0]
-}
-
-output "security_group_id" {
-  description = "ID of the web security group"
-  value       = module.web_sg.sg_id
-}
-
-output "web_server_id" {
-  description = "Web server instance ID"
-  value       = module.web_server.instance_id
-}
-
 output "web_server_ip" {
-  description = "Web server public IP"
+  description = "Public IP address of the web server"
   value       = module.web_server.public_ip
 }
 
-output "web_server_private_ip" {
-  description = "Web server private IP"
-  value       = module.web_server.private_ip
-}
-
-output "api_server_id" {
-  description = "API server instance ID"
-  value       = module.api_server.instance_id
-}
-
 output "api_server_ip" {
-  description = "API server public IP"
+  description = "Public IP address of the API server"
   value       = module.api_server.public_ip
 }
 
-output "api_server_private_ip" {
-  description = "API server private IP"
-  value       = module.api_server.private_ip
+output "security_group_id" {
+  description = "Web security group ID"
+  value       = module.web_sg.sg_id
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
 }
